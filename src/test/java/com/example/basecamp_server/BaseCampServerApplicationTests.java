b5746e8b253ed1a233d5f44dcf8910f35524f920
user <sooooooobinn@gmail.com>
2026-07-23 11:15:29 +0900

    init

---
+++ src/test/java/com/example/basecamp_server/BaseCampServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basecamp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCampServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
